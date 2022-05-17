70c2bacfec27d4c2f8530e94ea98d069de4ad819
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 22:34:24 +0200

    Update exercice_1-3_calcul-de-la-moyenne
    J'ai ajouté un int.Parse() pour récupérer les valeurs de nombre1 et nombre2.
    J'ai également ajouté un Console.WriteLine() pour vérifier les valeurs acquises dans nombre1 et nombre2.

--- DOSSIER_03_ALGORITHMIQUE/exercice_1-3_calcul-de-la-moyenne/exercice_1-3_calcul-de-la-moyenne/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_1-3_calcul-de-la-moyenne/exercice_1-3_calcul-de-la-moyenne/Program.cs
@@ -8,9 +8,11 @@ namespace exercice_1_3_calcul_de_la_moyenne
 
         {
             Console.Write("Veuillez saisir un premier nombre : ");
-            int nombre1 = Console.Read();
+            int nombre1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(nombre1);
             Console.Write("Veuillez saisir un second nombre : ");
-            int nombre2 = Console.Read();
+            int nombre2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(nombre2);
             float moyenne = (nombre1 + nombre2) / 2;
             Console.WriteLine("La moyenne de " + nombre1 + " et " + nombre2 + " est : " + moyenne);
         }
