d787908a35ac24793acdac8dcc74e49147c874c4
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 17:03:28 +0200

    Create Program.cs

---
+++ exo_algo_dc/convertisseur_celsius/convertisseur_celsius/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace convertisseur_celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir une tempchaineérature à convertir avec l'unité C/F : ");
+
+            string tempchaine = Console.ReadLine();
+            string unite = tempchaine.Substring(tempchaine.Length - 1, 1); //tempchaine[^2..]
+            if (unite == "C")
+            {
+                double temp = double.Parse(tempchaine.Substring(0, tempchaine.Length - 2));
+
+                temp = (temp * 9 / 5) + 32;
+                temp = Math.Round(temp, 2);
+
+                tempchaine = temp + " F";
+                Console.WriteLine("Farenheit : " + tempchaine);
+            }
+            else 
+            {
+                double temp = double.Parse(tempchaine.Substring(0, tempchaine.Length - 2));
+
+                temp = (temp - 32) * 5 / 9;
+                temp = Math.Round(temp, 2);
+                Console.WriteLine("Celsius : " + tempchaine);            
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
