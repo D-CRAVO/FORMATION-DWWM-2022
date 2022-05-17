765fc0e4e0b04783f11263b45921368b7e95de91
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 20:25:07 +0200

    Create exercice_1-2_hello-you

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_1-2_hello-you/exercice_1-2_hello-you/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercice_1_2_hello_you
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir votre prénom : ");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Bonjour " + prenom);
+        }
+    }
+}
