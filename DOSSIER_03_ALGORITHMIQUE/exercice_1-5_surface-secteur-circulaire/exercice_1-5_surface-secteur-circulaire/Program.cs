dc1e191c3077b298b7498891d85c7dc761261d9e
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 20:30:38 +0200

    Create exercice_1-5_surface-secteur-circulaire

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_1-5_surface-secteur-circulaire/exercice_1-5_surface-secteur-circulaire/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercice_1_5_surface_secteur_circulaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
