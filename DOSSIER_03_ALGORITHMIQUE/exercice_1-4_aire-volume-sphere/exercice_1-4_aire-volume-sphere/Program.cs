983f614e52619786fed8e8af325c6efca39c0ae1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 20:28:27 +0200

    Create exercice_1-5_surface-secteur-circulaire

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_1-4_aire-volume-sphere/exercice_1-4_aire-volume-sphere/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercice_1_4_aire_volume_sphere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Veuillez saisir le rayon de la sphère : ");
+            double rayon = Console.Read();
+            Console.WriteLine(rayon);
+            double aire = Math.Round((4 * Math.Pow(rayon, 2) * Math.PI),2);
+            Console.WriteLine("L'aire de la sphère est de " + aire);
+            double volume = Math.Round((4 / 3 * Math.PI * Math.Pow(rayon, 3)), 2);
+            Console.WriteLine("Le volume de la sphère est de " + volume);
+        }
+    }
+}
