1d6c9c5e06bc0df19c8f7090b1ef5ad02261f8be
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 20:26:56 +0200

    Create exercice_1-3_calcul-de-la-moyenne

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_1-3_calcul-de-la-moyenne/exercice_1-3_calcul-de-la-moyenne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercice_1_3_calcul_de_la_moyenne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Veuillez saisir un premier nombre : ");
+            int nombre1 = Console.Read();
+            Console.Write("Veuillez saisir un second nombre : ");
+            int nombre2 = Console.Read();
+            float moyenne = (nombre1 + nombre2) / 2;
+            Console.WriteLine("La moyenne de " + nombre1 + " et " + nombre2 + " est : " + moyenne);
+        }
+    }
+}
