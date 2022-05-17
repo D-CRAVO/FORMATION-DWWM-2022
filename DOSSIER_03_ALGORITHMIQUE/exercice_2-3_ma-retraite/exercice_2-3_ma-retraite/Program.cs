a460dbd403c94b419a67da4fe94caff014a78a7d
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-17 23:48:21 +0200

    Create exercice_2-3_ma-retraite

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_2-3_ma-retraite/exercice_2-3_ma-retraite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercice_2_3_ma_retraite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float age_retraite = 60;
+            Console.Write("Veuillez saisir votre âge : ");
+            float age = float.Parse(Console.ReadLine());
+            if (age<0)
+            {
+                Console.WriteLine("La valeur fournie n'est pas un âge valide");
+            }
+            else
+            {
+                if (age < age_retraite)
+                {
+                    float difference = age_retraite - age;
+                    Console.WriteLine("Il vous reste " + difference + " années avant la retraite.");
+                }
+                else
+                {
+                    if (age > age_retraite)
+                    {
+                        float difference = age - age_retraite;
+                        Console.WriteLine("Vous êtes à la retraite depuis " + difference + " années.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("C'est le moment de prendre votre retraite.");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
