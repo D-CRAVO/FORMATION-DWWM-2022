46a658416bc318547d7fc038d4416761a590a862
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-18 11:58:07 +0200

    Create exercice_3-3_intervalle

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_3-3_intervalle/exercice_3-3_intervalle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercice_3_3_intervalle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre_1;
+            int nombre_2;
+            int compteur;
+
+            Console.Write("Veuillez saisir votre premier nombre : ");
+            nombre_1 = int.Parse(Console.ReadLine());
+            Console.Write("Veuillez saisir votre second nombre : ");
+            nombre_2 = int.Parse(Console.ReadLine());
+            if (nombre_1 < nombre_2)
+            {
+                compteur = nombre_1 + 1;
+                while (compteur < nombre_2)
+                {
+                    Console.Write(compteur + " ");
+                    compteur++;
+                }
+            }
+            else
+            {
+                compteur = nombre_1 - 1;
+                while (compteur > nombre_2)
+                {
+                    Console.Write(compteur + " ");
+                    compteur--;
+                    
+                }
+            }
+        }
+    }
+}
