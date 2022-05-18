e0e6e236173fcab6ae8833276d0f59b0e6eef88f
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-18 16:32:28 +0200

    Create exercice_3-7_barnabe

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_3-7_barnabe/exercice_3-7_barnabe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercice_3_7_barnabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float somme;
+            int compteur;
+
+            compteur = 0;
+            somme = 0;
+
+            Console.Write("Veuillez entrer la somme de départ : ");
+            somme = float.Parse(Console.ReadLine());
+            while (somme > 1)
+            {
+                somme = somme - (somme/2 + 1);
+                compteur++;
+            }
+            Console.WriteLine("Barbabé a visité " + (compteur + 1) + " magasins.");
+        }
+    }
+}
