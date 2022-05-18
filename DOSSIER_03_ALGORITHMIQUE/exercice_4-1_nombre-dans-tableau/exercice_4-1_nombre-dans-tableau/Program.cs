aa8bf5e1c013edfec43479747667f904295f8e24
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-18 16:57:48 +0200

    Create exercice_4-1_nombre-dans-tableau

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_4-1_nombre-dans-tableau/exercice_4-1_nombre-dans-tableau/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercice_4_1_nombre_dans_tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int compteur;
+            int nombre;
+
+            int[] tableau = new int[] { 8, 16, 32, 64, 128, 256, 512 };
+            compteur = 0;
+
+            Console.WriteLine("Veuillez saisir un nombre entier : ");
+            nombre = int.Parse(Console.ReadLine());
+            while (compteur < tableau.Length)
+            {
+                if (tableau[compteur] == nombre)
+                {
+                    Console.WriteLine("L'indice du nombre " + nombre + " dans le tableau est : " + (compteur + 1));
+                    compteur = tableau.Length;
+                }
+                else
+                {
+                    compteur++;
+                }
+            }
+            Console.WriteLine("Nombre non trouvé.");
+
+        }
+    }
+}
