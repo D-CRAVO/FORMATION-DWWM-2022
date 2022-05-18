ee02e3c0b0fcb9576a8e8ca8ab006b4b022907cc
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-18 09:39:35 +0200

    Create exercice_3-1_controler-la-saisie

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_3-1_controler-la-saisie/exercice_3-1_controler-la-saisie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercice_3_1_controler_la_saisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenom;
+
+            do
+            {
+                Console.Write("Veuillez saisir votre prénom : ");
+                prenom = Console.ReadLine();
+            }
+            while (prenom.Length <= 2);
+            Console.WriteLine("Bonjour " + prenom);
+        }
+    }
+}
