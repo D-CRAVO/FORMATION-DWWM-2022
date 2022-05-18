33c3697187516af160abcc5d80dd0a331a2c5f58
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-18 12:17:42 +0200

    Create exercice_3-4_diviseurs

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_3-4_diviseurs/exercice_3-4_diviseurs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercice_3_4_diviseurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            int compteur;
+
+            Console.Write("Veuillez saisir votre premier nombre entier : ");
+            nombre = int.Parse(Console.ReadLine());
+            compteur = nombre - 1;
+            while (compteur >= 2)
+            {
+                if ((nombre % compteur) == 0)
+                {
+                    Console.Write(compteur + " ");
+                }
+                compteur--;
+            }
+        }
+    }
+}
