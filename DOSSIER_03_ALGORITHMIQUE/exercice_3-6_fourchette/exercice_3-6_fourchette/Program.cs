3b7670845bb156cd78ad04168622b416c0f8f99d
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-19 10:43:22 +0200

    Correction exercice_3-6_fourchette
    Frank a corrigé l'exercice au poste de travail

--- DOSSIER_03_ALGORITHMIQUE/exercice_3-6_fourchette/exercice_3-6_fourchette/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_3-6_fourchette/exercice_3-6_fourchette/Program.cs
@@ -6,35 +6,40 @@ namespace exercice_3_6_fourchette
     {
         static void Main(string[] args)
         {
-            int nombre;
+            int nombre_machine;
             int limite_inferieure;
             int limite_superieure;
             int compteur;
             int nombre_saisi;
-
-            nombre = 27;
+            Random aleas = new Random();
+            nombre_machine = aleas.Next(0,101);
             compteur = 0;
             nombre_saisi = 0;
             limite_inferieure = 0;
             limite_superieure = 100;
 
-            Console.WriteLine(nombre);
-            while (nombre_saisi != nombre)
+            //  Console.WriteLine(nombre);
+            do
             {
-                Console.WriteLine("Veuillez saisir un nombre entier : ");
+                Console.WriteLine("Nombre d'essais : " + compteur);
+                Console.WriteLine("Veuillez saisir un nombre entier entre : " + limite_inferieure + " et " + limite_superieure);
+
                 nombre_saisi = int.Parse(Console.ReadLine());
-                if (nombre < nombre_saisi)
+                if (nombre_machine < nombre_saisi)
                 {
                     limite_superieure = nombre_saisi;
+                    compteur++;
+                    
                 }
                 else
                 {
                     limite_inferieure = nombre_saisi;
+                    compteur++;
                 }
                 Console.WriteLine("Le nombre est compris entre " + limite_inferieure + " et " + limite_superieure);
-                compteur++;
-            }
-            Console.WriteLine("Bravo, vous avez trouvé en " + compteur + " essais.");
+               
+            } while (nombre_saisi != nombre_machine);
+            Console.WriteLine("Bravo, vous avez trouvé en " + compteur + " essais. C'était le nombre : " + nombre_saisi);
         }
     }
 }
