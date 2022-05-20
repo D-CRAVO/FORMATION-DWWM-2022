eddf87e9b3442b2ad587888cf97677b4a96c7e31
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-20 11:54:13 +0200

    Create exercice_4-5_palindrome

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_4-5_palindrome/exercice_4-5_palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿// VARIABLES
+
+string phrase;
+
+int compteur;
+
+char[] tableau_phrase = new char[] { };
+
+do
+{
+    Console.WriteLine("Veuillez saisir votre phrase : ");
+    phrase = Console.ReadLine();
+    tableau_phrase = phrase.ToCharArray();
+} while ((tableau_phrase[phrase.Length - 1] != '.') && (phrase.Length <= 1));
+
+for (compteur = 0; compteur < phrase.Length; compteur++)
+{
+
+}
