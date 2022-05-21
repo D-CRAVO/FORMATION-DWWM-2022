643fe715b008e50c6992f23fdad5cf3ac49352c5
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-21 13:05:44 +0200

    Update exercice_4-5_palindrome
    J'ai achevé le codage en C Sharp commencé en cours.

--- DOSSIER_03_ALGORITHMIQUE/exercice_4-5_palindrome/exercice_4-5_palindrome/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_4-5_palindrome/exercice_4-5_palindrome/Program.cs
@@ -1,19 +1,72 @@
 ﻿// VARIABLES
 
+string end;
 string phrase;
-
 int compteur;
 
-char[] tableau_phrase = new char[] { };
+char[] tableau_phrase = new char[] {};
+string[] tableau_caractere_indesirable = new string[] { "@", ",", ".", ";", "'", " " };
 
+
+// ASSIGNATIONS
+end = "Traitement terminé";
+compteur = 0;   
+
+// DEBUT PROGRAMME
+
+// On vérifie d'abord que la phrase comporte au moins un caractère et est terminée par un point.
 do
 {
-    Console.WriteLine("Veuillez saisir votre phrase : ");
+    Console.WriteLine("Veuillez saisir votre phrase avec au moins un caractère et terminée par un point : ");
     phrase = Console.ReadLine();
     tableau_phrase = phrase.ToCharArray();
-} while ((tableau_phrase[phrase.Length - 1] != '.') && (phrase.Length <= 1));
+    
+    if (tableau_phrase.Length == 0)
+    {
+        tableau_phrase = " ".ToCharArray();
+        Console.WriteLine("Votre phrase doit contenir au moins un caractère.");
+    }
+    else
+    {
+        if (tableau_phrase[0] == '.')
+        {
+            Console.WriteLine("Votre phrase doit contenir au moins un caractère.");
+        }
+        else
+        {
+            if (tableau_phrase[tableau_phrase.Length - 1] != '.')
+            {
+                Console.WriteLine("Votre phrase doit se terminer par un point.");
+            }
+        }
+    }
 
-for (compteur = 0; compteur < phrase.Length; compteur++)
-{
 
+} while ((tableau_phrase[tableau_phrase.Length-1] != '.') || (tableau_phrase.Length <2));
+
+// On supprime les caractères indésirables.
+foreach (var caractere_indesirable in tableau_caractere_indesirable)
+{ 
+    phrase = phrase.Replace(caractere_indesirable, string.Empty);
 }
+
+// On converti les majuscules en minuscules dans la phrase.
+phrase = phrase.ToLower();
+tableau_phrase = phrase.ToCharArray();
+
+// On compare la première et la dernière lettre pour voir si elles sont identiques.
+// Puis la deuxième et l'avant dernière et ainsi de suite jusqu'à arriver au milieu de la phrase.
+do
+{
+    if (tableau_phrase[compteur] == tableau_phrase[tableau_phrase.Length - 1 - compteur])
+    {
+        end = "La phrase est un palindrome.";
+    }
+    else
+    {
+        end = "La phrase n'est pas un palindrome.";
+    }
+    compteur++;
+} while (tableau_phrase[compteur] < tableau_phrase[tableau_phrase.Length - 1 - compteur]);
+
+Console.WriteLine(end);
