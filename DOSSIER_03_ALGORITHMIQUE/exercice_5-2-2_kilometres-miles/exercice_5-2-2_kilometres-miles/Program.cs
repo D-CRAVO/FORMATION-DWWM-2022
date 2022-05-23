9aa90f8f028c917c45da429054e35b3253d5d6a1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-23 16:43:21 +0200

    Create exercice_5-2-2_kilometres-miles

--- DOSSIER_03_ALGORITHMIQUE/exercice_5-2-2_kilometres-miles/exercice_5-2-2_kilometres-miles/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_5-2-2_kilometres-miles/exercice_5-2-2_kilometres-miles/Program.cs
@@ -1,5 +1,5 @@
 ﻿// VARIABLES
-string valeur_saisie = "";
+string valeur_saisie = "  ";
 string unite = "km";
 string end = "Aurevoir !";
 
@@ -12,8 +12,10 @@ double valeur_maximale = 1000000;
 // PROGRAMME
 do
 {
+    // On récupère la valeur à convertir.
     Console.WriteLine("Veuillez saisir une valeur en kimomètres comprise entre " + valeur_minimale + " et " + valeur_maximale + " : ");
     valeur_saisie = Console.ReadLine().ToLower();
+    // On vérifie si l'utilisateur veut quitter l'application.
     if (valeur_saisie == "q")
     {
         Console.WriteLine(end);
@@ -21,33 +23,44 @@ do
     }
     else
     {
-        unite = valeur_saisie.Substring(valeur_saisie.Length, 2);
+        // On vérifie les deux derniers caractères saisis pour connaitre l'unité.
+        unite = valeur_saisie.Substring(valeur_saisie.Length-2, 2);
         if (unite == "mi" )
         {
-            valeur_kilometres = double.Parse(valeur_saisie);
-            if (valeur_kilometres <= valeur_minimale)
+            valeur_miles = double.Parse(valeur_saisie.Substring(0,valeur_saisie.Length-2));
+            // On vérifie les conditions d'intervalle.
+            if (valeur_miles < valeur_minimale)
             {
                 Console.WriteLine("Vous êtes en dessous de la valeur minimale " + valeur_minimale + " .");
             }
             else
             {
-                if (valeur_kilometres >= valeur_maximale)
+                if (valeur_miles >= valeur_maximale)
                 {
                     Console.WriteLine("Vous êtes au dessus de la valeur maximale " + valeur_maximale + " .");
                 }
                 else
                 {
-                    valeur_miles = 1.609 * valeur_kilometres;
-                    Console.WriteLine("La conversion de " + valeur_kilometres + " kilomètres en miles donne : " + valeur_miles);
+                    // On converti les miles en kilomètre par défaut.
+                    valeur_kilometres = valeur_miles / 1.609;
+                    Console.WriteLine("La conversion de " + valeur_miles + " miles en kilomètres donne : " + valeur_kilometres + " kms");
                 }
             }
         }
         else
         {
-            valeur_kilometres = valeur_miles / 1.609;
+            if (unite == "km")
+            {
+                valeur_kilometres = double.Parse(valeur_saisie.Substring(0, valeur_saisie.Length - 2));
+            }
+            else
+            {
+                valeur_kilometres = double.Parse(valeur_saisie.Substring(0, valeur_saisie.Length));
+            }
+            // On convertit les kilomètres en miles.
+            valeur_miles = 1.609 * valeur_kilometres;
+            Console.WriteLine("La conversion de " + valeur_kilometres + " kilomètres en miles donne : " + valeur_miles + " miles");
         }
-
     }
-
 } while (valeur_saisie != "q" || valeur_kilometres <= valeur_minimale || valeur_kilometres >= valeur_maximale);
 
