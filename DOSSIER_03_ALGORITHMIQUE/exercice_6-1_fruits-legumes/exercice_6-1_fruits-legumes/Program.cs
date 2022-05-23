8860edc970f2cc58be1dda6bd05ff1e762ae42d5
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-23 16:59:11 +0200

    Create exercice_6-1_fruits-legumes

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
@@ -0,0 +1,8 @@
+﻿// VARIABLES
+
+
+// PROGRAMME
+
+Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace");
+legume_et_prix = Console.ReadLine();
+
