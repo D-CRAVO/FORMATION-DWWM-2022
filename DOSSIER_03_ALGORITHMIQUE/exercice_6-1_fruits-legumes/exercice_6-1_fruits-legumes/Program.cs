2639863015d4ed7c39df9ef4562fff0378e8e10a
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-23 23:10:00 +0200

    Update exercice_6-1_fruits-legumes
    J'ai commencé à coder la phase d'acquisition des données.

--- DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
@@ -1,8 +1,46 @@
 ﻿// VARIABLES
+string legume_et_prix;
+string end = "Traitement terminé";
+int compteur = 0 ;
+
+char[] separateurs = new char[] { ' ' };
+string[,] legume_prix_separes = new string[,] { };
 
 
 // PROGRAMME
 
-Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace");
-legume_et_prix = Console.ReadLine();
+
+
+
+
+do
+{
+    // On suppose dans un premier temps que les noms des légumes sont écrits sans espaces.
+    Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace");
+    legume_et_prix = Console.ReadLine();
+
+    if (legume_et_prix != "go")
+    {
+        // On découpe la variable legume_et_prix en deux parties afin de récupérer le nom du légume et son prix
+        // séparément dans un tableau à deux dimensions.
+        string[] legume_prix = legume_et_prix.Split(separateurs, StringSplitOptions.RemoveEmptyEntries);
+        legume_prix_separes =  new string[,] { { legume_prix[compteur], legume_prix[1] } };
+
+        // On affiche le tableau à deux dimensions obtenu.
+        Console.WriteLine("On affiche le tableau à deux dimensions obtenu : ");
+        Console.WriteLine(legume_prix_separes[compteur, 0] + " " + legume_prix_separes[compteur, 1]);
+        compteur++;
+    }
+    else
+    {
+        Console.WriteLine(end);
+        break;
+    }
+
+  
+} while (legume_et_prix.ToLower() != "go");
+
+
+
+
 
