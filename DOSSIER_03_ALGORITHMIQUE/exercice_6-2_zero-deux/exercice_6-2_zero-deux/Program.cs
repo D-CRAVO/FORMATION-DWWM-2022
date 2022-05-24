2e270e9f9a4718f24ffbcaca519556a2f0e52d47
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-24 20:03:21 +0200

    Update exercice_6-2-zero-deux
    J'ai recherché comment sortir de la boucle DO WHILE sans succès.
    Je ferme tout et redémarre le pc pour voir si Visual Studio 2022 recompile.
    Le logiciel s'arrête également et se bloque.

--- DOSSIER_03_ALGORITHMIQUE/exercice_6-2_zero-deux/exercice_6-2_zero-deux/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-2_zero-deux/exercice_6-2_zero-deux/Program.cs
@@ -4,6 +4,7 @@ int nombre_ordinateur;
 int difference = 0;
 int compteur_joueur = 0;
 int compteur_ordinateur = 0;
+int nombre_points = 5;
 
 string end = "Traitement terminé";
 
@@ -13,14 +14,14 @@ do
 {
     // On récupère la valeur aléatoire de l'ordinateur 
     Random random = new Random();
-    nombre_ordinateur = random.Next(0, 2);
+    nombre_ordinateur = random.Next(0, 3);
 
     // On récupère la saisie de l'utilisateur 
-    Console.WriteLine("Veuillez saisir votre choix(0, 1, 2) ou - 1 pour quitter: ");
+    Console.Write("Veuillez saisir votre choix(0, 1, 2) ou - 1 pour quitter: ");
     nombre_joueur = int.Parse(Console.ReadLine());
 
     // On vérifie si l'utilisateur veut sortir en saisissant un nombre négatif.
-    if (nombre_joueur>= 0)
+    if (nombre_joueur >= 0)
     {
         difference = Math.Abs(nombre_joueur - nombre_ordinateur);
     }
@@ -53,5 +54,19 @@ do
             compteur_ordinateur++;
         }
     }
-} while ((nombre_joueur >= 0) || (compteur_joueur >= 5) || (compteur_ordinateur >= 5));
+} while ((nombre_joueur > 0) || (compteur_joueur != nombre_points) || (compteur_ordinateur != nombre_points));
 
+// On regarde lequel du joueur ou de l'ordinateur gagne.
+if (compteur_joueur == nombre_points)
+{
+    Console.WriteLine("Le joueur gagne !");
+    Console.WriteLine("Le score est de " + nombre_points + " à " + compteur_ordinateur);
+}
+else 
+{
+    if (compteur_ordinateur == nombre_points)
+    {
+        Console.WriteLine("L'ordinateur gagne !");
+        Console.WriteLine("Le score est de " + nombre_points + " à " + compteur_joueur);
+    }
+}
