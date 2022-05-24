bd29656c334bb6afdda0d82f93e9a6ecc6a50e01
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-24 12:51:31 +0200

    Update exercice_6-1_fruits-legumes
    La boucle WHILE de récupération des données a été achevée.
    La boucle FOR de l'affichage des légumes et de leur prix au kilo a été créée.
    La boucle For de détermination du prix le plus faible  a été créée.

--- DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
@@ -1,52 +1,59 @@
 ﻿// VARIABLES
-string legume_et_prix;
+string legume_et_prix_saisi;
+double prix_le_moins_cher;
 string end = "Traitement terminé";
 int compteur = 0 ;
+int index = 0 ;
 
 char[] separateurs = new char[] { ' ' };
-string[,] legume_prix_separes = new string[,] { };
-string[,] nouveau_legume_prix_separes = new string[,] { };
+
+List<string> nouveau_legume_nom = new List<string>();
+List<string> nouveau_legume_prix = new List<string>();
 
 // PROGRAMME
 
 // On suppose dans un premier temps que les noms des légumes sont écrits sans espaces.
-Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace : ");
-legume_et_prix = Console.ReadLine(); 
-
 do
 {
     // On suppose dans un premier temps que les noms des légumes sont écrits sans espaces.
     Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace : ");
-    legume_et_prix = Console.ReadLine();
+    legume_et_prix_saisi = Console.ReadLine();
 
-    if (legume_et_prix.ToLower() != "go")
+    if (legume_et_prix_saisi.ToLower() != "go")
     {
         // On découpe la variable legume_et_prix en deux parties afin de récupérer le nom du légume et son prix
-        // séparément dans un tableau à deux dimensions.
-        string[] legume_prix = legume_et_prix.Split(separateurs, StringSplitOptions.RemoveEmptyEntries);
-        nouveau_legume_prix_separes = new string[,] { { legume_prix[compteur], legume_prix[1] } };
-        legume_prix_separes =  legume_prix_separes.Concat(nouveau_legume_prix_separes).ToArray();
+        string[] legume_prix = (legume_et_prix_saisi.Split(separateurs, StringSplitOptions.RemoveEmptyEntries));
+
+        nouveau_legume_nom.Add(legume_prix[0]);
+        nouveau_legume_prix.Add(legume_prix[1]);
 
         // Rechercher char[][] JaggedCharArray = new char[][]
         // https://codes-sources.commentcamarche.net/forum/affich-1346404-tableau-a-2-dimensions-en-c
         // https://stackoverflow.com/questions/1547252/how-do-i-concatenate-two-arrays-in-c
         // https://www.delftstack.com/fr/howto/csharp/merge-two-arrays-in-csharp/
 
-        // On affiche le tableau à deux dimensions obtenu.
-        Console.WriteLine("On affiche le tableau à deux dimensions obtenu : ");
-        Console.WriteLine(legume_prix_separes[compteur, 0] + " " + legume_prix_separes[compteur, 1]);
-        compteur++;
     }
-    else
+} while (legume_et_prix_saisi.ToLower() != "go");
+
+
+// Affichage du nom du légume et de son prix au kilo.
+for (compteur = 0; compteur < nouveau_legume_nom.Count; compteur++)
+{
+    Console.WriteLine("1 kilogramme de " + nouveau_legume_nom[compteur] + " coute " + nouveau_legume_prix[compteur] + " euros.");
+}
+
+
+// Recherche et affichage du moins cher
+prix_le_moins_cher = double.Parse(nouveau_legume_prix[0]);
+for (compteur = 0; compteur < nouveau_legume_nom.Count; compteur++)
+{
+    if (double.Parse(nouveau_legume_prix[compteur]) < prix_le_moins_cher)
     {
-        Console.WriteLine(end);
-        break;
+        prix_le_moins_cher = double.Parse(nouveau_legume_prix[compteur]);
+        index = compteur;
     }
+}
 
-  
-} while (legume_et_prix.ToLower() != "go");
-
-
-
-
-
+Console.WriteLine("Prix au kilo le moins cher : " + prix_le_moins_cher + " euros");
+Console.WriteLine("Légume le moins cher au kilo : " + nouveau_legume_nom[index]);
+Console.WriteLine(end);
