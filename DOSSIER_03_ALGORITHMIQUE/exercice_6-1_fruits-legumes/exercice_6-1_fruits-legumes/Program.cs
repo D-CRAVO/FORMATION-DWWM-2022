46410d0cb12341bda07bc856d6b72ef33beb396e
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-24 07:42:23 +0200

    Update exercice_6-1_fruits-legumes
    Je recherche comment concaténer deux tableaux à deux dimensions.

--- DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-1_fruits-legumes/exercice_6-1_fruits-legumes/Program.cs
@@ -5,26 +5,32 @@ int compteur = 0 ;
 
 char[] separateurs = new char[] { ' ' };
 string[,] legume_prix_separes = new string[,] { };
-
+string[,] nouveau_legume_prix_separes = new string[,] { };
 
 // PROGRAMME
 
-
-
-
+// On suppose dans un premier temps que les noms des légumes sont écrits sans espaces.
+Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace : ");
+legume_et_prix = Console.ReadLine(); 
 
 do
 {
     // On suppose dans un premier temps que les noms des légumes sont écrits sans espaces.
-    Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace");
+    Console.WriteLine("Veuillez saisir le nom de votre légume suivi de son prix au kilo séparé par un espace : ");
     legume_et_prix = Console.ReadLine();
 
-    if (legume_et_prix != "go")
+    if (legume_et_prix.ToLower() != "go")
     {
         // On découpe la variable legume_et_prix en deux parties afin de récupérer le nom du légume et son prix
         // séparément dans un tableau à deux dimensions.
         string[] legume_prix = legume_et_prix.Split(separateurs, StringSplitOptions.RemoveEmptyEntries);
-        legume_prix_separes =  new string[,] { { legume_prix[compteur], legume_prix[1] } };
+        nouveau_legume_prix_separes = new string[,] { { legume_prix[compteur], legume_prix[1] } };
+        legume_prix_separes =  legume_prix_separes.Concat(nouveau_legume_prix_separes).ToArray();
+
+        // Rechercher char[][] JaggedCharArray = new char[][]
+        // https://codes-sources.commentcamarche.net/forum/affich-1346404-tableau-a-2-dimensions-en-c
+        // https://stackoverflow.com/questions/1547252/how-do-i-concatenate-two-arrays-in-c
+        // https://www.delftstack.com/fr/howto/csharp/merge-two-arrays-in-csharp/
 
         // On affiche le tableau à deux dimensions obtenu.
         Console.WriteLine("On affiche le tableau à deux dimensions obtenu : ");
