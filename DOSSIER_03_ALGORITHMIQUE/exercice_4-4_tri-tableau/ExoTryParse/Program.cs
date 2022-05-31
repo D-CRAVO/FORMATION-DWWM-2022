a52097b258bbfd0683b778f11fd4d5325f1dfb51
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-05-31 16:40:07 +0200

    Create ExoTryParse

---
+++ DOSSIER_03_ALGORITHMIQUE/exercice_4-4_tri-tableau/ExoTryParse/Program.cs
@@ -0,0 +1,13 @@
+﻿// VARIABLE
+
+uint nombre;
+bool ok = false;
+do
+{
+    Console.WriteLine("Donnez moi un nombre");
+    ok = uint.TryParse(Console.ReadLine(), out nombre);
+    if (!ok)
+    {
+        Console.WriteLine("Le nombre saisi n'est pas correct");
+    }
+} while (!ok);
