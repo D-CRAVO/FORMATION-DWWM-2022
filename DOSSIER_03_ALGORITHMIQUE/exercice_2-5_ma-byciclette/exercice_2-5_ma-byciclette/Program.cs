3620d6f18a46b15be31b8ca5e310e931084c1472
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-01 15:05:12 +0200

    Update exercice_2-5_ma-byciclette

--- DOSSIER_03_ALGORITHMIQUE/exercice_2-5_ma-byciclette/exercice_2-5_ma-byciclette/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_2-5_ma-byciclette/exercice_2-5_ma-byciclette/Program.cs
@@ -10,17 +10,17 @@ bool livre_est_a_la_bibliotheque;
 
 // DEBUT PROGRAMME
 
-Console.WriteLine("Fait-il beau ? (true pour oui / false pour non)");
-il_fait_beau = bool.Parse(Console.ReadLine());
+Console.WriteLine("Fait-il beau ? ( oui / non )");
+il_fait_beau = bool.Parse(Console.ReadLine().Replace("oui","true").Replace("non","false"));
 if (il_fait_beau)
 {
-    Console.WriteLine("La bicyclette est-elle en mauvais état ? (true pour oui / false pour non)");
-    bicyclette_est_en_mauvais_etat = bool.Parse(Console.ReadLine());
+    Console.WriteLine("La bicyclette est-elle en mauvais état ? ( oui / non )");
+    bicyclette_est_en_mauvais_etat = bool.Parse(Console.ReadLine().Replace("oui", "true").Replace("non", "false"));
     if (bicyclette_est_en_mauvais_etat)
     {
         Console.WriteLine("Vous allez chez le garagiste");
-        Console.WriteLine("La réparation est-elle immédiate ? (true pour oui / false pour non)");
-        reparation_est_immediate = bool.Parse(Console.ReadLine());
+        Console.WriteLine("La réparation est-elle immédiate ? ( oui / non )");
+        reparation_est_immediate = bool.Parse(Console.ReadLine().Replace("oui", "true").Replace("non", "false"));
         if (reparation_est_immediate)
         {
             Console.WriteLine("Je vais faire de la bicyclette.");
@@ -38,8 +38,8 @@ if (il_fait_beau)
 else
 {
     Console.WriteLine("Vous voulez lire Game Of Thrones.");
-    Console.WriteLine("Le livre est-il dans votre salon ? (true pour oui / false pour non)");
-    livre_est_dans_le_salon = bool.Parse(Console.ReadLine());
+    Console.WriteLine("Le livre est-il dans votre salon ? ( oui / non )");
+    livre_est_dans_le_salon = bool.Parse(Console.ReadLine().Replace("oui", "true").Replace("non", "false"));
     if (livre_est_dans_le_salon)
     {
         Console.WriteLine("Vous relisez Game Of Thrones.");
@@ -47,8 +47,8 @@ else
     else
     {
         Console.WriteLine("Vous allez à la bibliothèque.");
-        Console.WriteLine("Le livre est-il disponible à la bibliothèque ? (true pour oui / false pour non)");
-        livre_est_a_la_bibliotheque = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Le livre est-il disponible à la bibliothèque ? ( oui / non )");
+        livre_est_a_la_bibliotheque = bool.Parse(Console.ReadLine().Replace("oui", "true").Replace("non", "false"));
         if (livre_est_a_la_bibliotheque)
         {
             Console.WriteLine("Vous rentrez à la maison.");
