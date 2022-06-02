b3104d88475296f00e87c977b327895c7bc08bc3
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-03 01:01:53 +0200

    Update exercice_4-3_lettre-alphabet

--- DOSSIER_03_ALGORITHMIQUE/exercice_4-3_lettre-alphabet/exercice_4-3_lettre-alphabet/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_4-3_lettre-alphabet/exercice_4-3_lettre-alphabet/Program.cs
@@ -9,37 +9,36 @@ namespace exercice_4_3_lettre_alphabet
             // EXERCICE 4.3 DENOMBRER LES LETTRES DE L'ALPHABET DANS UN TABLEAU
 
             // DECLARATION VARIABLES
+
             string texte;
             string alphabet = "";
-            string end = "Traitement terminé.";
 
+            int nombre_caractere = 5;
             int compteur_occurence;
             int compteur_texte;
             int compteur_alphabet;
 
             char[] tableau_texte;
-            char[] tableau_alphabet;
+            char[] tableau_alphabet = new char[26];
 
             char a = 'a';
 
             // DEBUT PROGRAMME
 
             // On charge l'alphabet dans le tableau alphabet.
-            for (compteur_alphabet = 0; compteur_alphabet < 26; compteur_alphabet++)
+            for (compteur_alphabet = 0; compteur_alphabet < tableau_alphabet.Length; compteur_alphabet++)
             {
                 alphabet += a++;
             }
             tableau_alphabet = alphabet.ToCharArray();
 
-            // alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-
             // On demande à l'utilisateur de saisir un texte de 120 caractères minimum.
             // On boucle tant que ce n'est pas le cas.
             do
             {
-                Console.WriteLine("Veuillez saisir votre texte de 120 caractères minimum: ");
+                Console.WriteLine("Veuillez saisir votre texte de " + nombre_caractere + " caractères minimum: ");
                 texte = Console.ReadLine().ToLower();
-            } while (texte.Length < 5);
+            } while (texte.Length < nombre_caractere);
             tableau_texte = texte.ToCharArray();
                   
             // On recherche le nombre d'occurences de chaque lettre de l'alphabet.
@@ -53,9 +52,11 @@ namespace exercice_4_3_lettre_alphabet
                         compteur_occurence++;
                     }
                 }
-                Console.WriteLine("La lettre " + tableau_alphabet[compteur_alphabet] + " apparaît " + compteur_occurence + " fois dans le texte.");
+                if (compteur_occurence != 0)
+                {
+                    Console.WriteLine("La lettre " + tableau_alphabet[compteur_alphabet] + " apparaît " + compteur_occurence + " fois dans le texte.");
+                }
             }
-            Console.WriteLine(end);
         }
     }
 }
