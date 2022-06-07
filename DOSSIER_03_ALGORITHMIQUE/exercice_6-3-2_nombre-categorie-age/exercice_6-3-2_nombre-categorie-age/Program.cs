212e205a8bfe94500ccb43d9bcecc33cf10d6e68
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-07 18:20:52 +0200

    Update exercice_6-3-2_nombre-categorie-age

--- DOSSIER_03_ALGORITHMIQUE/exercice_6-3-2_nombre-categorie-age/exercice_6-3-2_nombre-categorie-age/Program.cs
+++ DOSSIER_03_ALGORITHMIQUE/exercice_6-3-2_nombre-categorie-age/exercice_6-3-2_nombre-categorie-age/Program.cs
@@ -4,50 +4,40 @@
 
 // VARIABLES
 
-int nombre_personnes;
-int age_jeune;
-int moins_age_jeune;
-int egal_age_jeune;
-int plus_age_jeune;
+int nombre_personnes = 20;
+int age_jeune = 20;
+int moins_age_jeune = 0;
+int egal_age_jeune = 0;
+int plus_age_jeune = 0;
 int compteur;
 
-int[] age_personne = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+double[] age_personne = new double[nombre_personnes];
 
 // DEBUT PROGRAMME
 
-// On détermine le nombre de personnes de l'échantillon.
-nombre_personnes = 20;
-
-// On détermine l'age en dessous duquel la personne est jeune.
-age_jeune = 20;
-
-moins_age_jeune = 0;
-egal_age_jeune = 0;
-plus_age_jeune = 0;
-compteur = 0;
+// On récupère le nombre de personnes de l'échantillon.
+Console.Write("Veuillez indiquer le nombre de personnes de l'échantillon : ");
+nombre_personnes = int.Parse(Console.ReadLine());
 
 // On récupère et on analyse l'âge des personnes.
 for (compteur = 0; compteur < nombre_personnes; compteur++)
 {
     // On affiche (compteur + 1) pour ne pas perturber l'utilisateur.
     Console.Write("Veuillez saisir l'âge de la personne" + (compteur + 1) + " : ");
-    age_personne[compteur] = int.Parse(Console.ReadLine());
+    age_personne[compteur] = double.Parse(Console.ReadLine());
 
     // On contrôle l'age de la personne.
     if (age_personne[compteur] < age_jeune)
     {
         moins_age_jeune++;
     }
+    else if (age_personne[compteur] > age_jeune)
+    {
+        plus_age_jeune++;
+    }
     else
     {
-        if (age_personne[compteur] > age_jeune)
-        {
-            plus_age_jeune++;
-        }
-        else
-        {
-            egal_age_jeune++;
-        }
+        egal_age_jeune++;
     }
 }
 
@@ -56,16 +46,13 @@ if (moins_age_jeune == nombre_personnes)
 {
     Console.WriteLine("Toutes les personnes ont moins de " + age_jeune + " ans.");
 }
+else if (plus_age_jeune + egal_age_jeune == nombre_personnes)
+{
+    Console.WriteLine("Toutes les personnes ont plus de " + age_jeune + " ans.");
+}
 else
 {
-    if (plus_age_jeune + egal_age_jeune == nombre_personnes)
-    {
-        Console.WriteLine("Toutes les personnes ont plus de " + age_jeune + " ans.");
-    }
-    else
-    {
-        Console.WriteLine(moins_age_jeune + " personnes ont moins de " + age_jeune + " ans.");
-        Console.WriteLine(egal_age_jeune + " personnes ont " + age_jeune + " ans.");
-        Console.WriteLine(plus_age_jeune + " personnes ont plus de " + age_jeune + " ans.");
-    }
+    Console.WriteLine(moins_age_jeune + " personnes ont moins de " + age_jeune + " ans.");
+    Console.WriteLine(egal_age_jeune + " personnes ont " + age_jeune + " ans.");
+    Console.WriteLine(plus_age_jeune + " personnes ont plus de " + age_jeune + " ans.");
 }
