f0a5f5b8bfdfcd59f7f7f56f208647eec151d821
David <toto@crm.fr>
2022-06-15 16:35:20 +0200

    Create Revisions exercice-partie-4

---
+++ DOSSIER_03_ALGORITHMIQUE/Revisions/exercice-partie-4/ex4-3-alphabet/Program.cs
@@ -0,0 +1,16 @@
+﻿// EXERCICE 4.3 DENOMBRER LES LETTRES DE L'ALPHABET DANS UN TABLEAU
+
+// VARIABLES
+
+string phrase;
+int longueur_phrase = 5;
+
+// DEBUT PROGRAMME
+
+do
+{
+    // Récupération des données utilisateur.
+    Console.WriteLine("Veuillez saisir votre phrase de plus de " + longueur_phrase + " : ");
+    phrase = Console.ReadLine();
+} while (phrase.Length < longueur_phrase);
+
