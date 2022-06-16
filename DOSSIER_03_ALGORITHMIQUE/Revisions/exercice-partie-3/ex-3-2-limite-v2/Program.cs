66c4f8ed008db4f618c6ba57da1fad591f66ab89
David <toto@crm.fr>
2022-06-16 12:27:42 +0200

    Ajout du cours sur les tableaux

---
+++ DOSSIER_03_ALGORITHMIQUE/Revisions/exercice-partie-3/ex-3-2-limite-v2/Program.cs
@@ -0,0 +1,31 @@
+﻿// EXERCICE 3.2 CONTROLER LA SAISIE AVEC LIMITE
+
+// VARIABLES
+
+string mot_de_passe_enregistre = "formation";
+string mot_de_passe_saisi;
+bool mot_de_passe_correct;
+byte nombre_essais = 3;
+
+// DEBUT PROGRAMME
+
+do
+{
+    Console.WriteLine("Veuillez saisir votre mot de passe : ");
+    mot_de_passe_saisi = Console.ReadLine();
+
+    mot_de_passe_correct = ( mot_de_passe_enregistre == mot_de_passe_enregistre );
+    if (!mot_de_passe_correct)
+    {
+        nombre_essais--;
+    }
+} while (!mot_de_passe_correct && nombre_essais > 0);
+
+if (!mot_de_passe_correct)
+{
+    Console.WriteLine("Compte bloqué !");
+}
+else
+{
+    Console.WriteLine("Vous êtes connecté !");
+}
---
+++ DOSSIER_03_ALGORITHMIQUE/Revisions/exercice-partie-4/cours_tableau/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// collection de données
+
+int variable = 1;
+int[] tableau;
+tableau = new int[10];
+tableau[4] = 1337;
+tableau[7] = 42;
+int emplacement4 = tableau[4];
+emplacement4 = 1111;
+Console.WriteLine(emplacement4);
+Console.WriteLine(tableau[4]);
+
+string[] pays;
+pays = new string[]
+{
+    "France",
+    "Allemagne",
+    "Maroc",
+    "Belgique"
+};
+
+//Console.WriteLine(pays[1]);
+//pays[1] = "Autriche";
+//Console.WriteLine(pays[1]);
+
+for (int i = 0; i < pays.Length; i++)
+{
+    Console.WriteLine(i + " = " + pays[i]);
+}
+
+string[] resultat = new string[pays.Length - 1];
+
+int j = 0;
+for (int i = 0; i < pays.Length; i++)
+{
+    if (pays[i] != "Allemagne")
+    {
+        resultat[j] = pays[i];
+        j++;
+    }
+    Console.WriteLine(resultat[j]);
+}
+
+//for (int i = 0; i < tableau.Length; i++)
+//{
+//    Console.WriteLine(i + " = " + tableau[i]);
+//}
