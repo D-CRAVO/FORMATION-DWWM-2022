68347d829d789ab606d9a0cccaed9aa7b10d71d1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-17 17:00:06 +0200

    Update ex_2-3_retraite_v1

--- DOSSIER_05_PHP/ExercicesPHP/partie_2/ex_2-3_retraite_v1.php
+++ DOSSIER_05_PHP/ExercicesPHP/partie_2/ex_2-3_retraite_v1.php
@@ -6,19 +6,20 @@
     if ($age > $retraite)
     {
         $annees = $age - $retraite;
-        echo "Vous êtes à la retraite depuis $annees années.";
+        $resultat = "Vous êtes à la retraite depuis $annees années.";
     }
     elseif($age === $retraite)
     {
-        echo "C'est le moment de prendre votre retraite.";
+        $resultat = "C'est le moment de prendre votre retraite.";
     }
     elseif($age > 0)
     {
         $annees = $retraite - $age;
-        echo "Il vous reste $annees années avant la retraite.";
+        $resultat = "Il vous reste $annees années avant la retraite.";
     }
     else
     {
-        echo "La valeur fournie n'est pas un âge valide.";
+        $resultat = "La valeur fournie n'est pas un âge valide.";
     }
+    echo $resultat
 ?>
