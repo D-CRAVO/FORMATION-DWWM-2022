b991d82ba300b396d96a882def3c354a4939d672
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-17 13:06:49 +0200

    Create ex_1-3_moyenne.php

---
+++ DOSSIER_05_PHP/ExercicesPHP/partie_1/ex_1-3_moyenne.php
@@ -0,0 +1,6 @@
+<?php
+    $nb1 = readline('Veuillez saisir votre premier nombre entier : ');
+    $nb2 = readline('Veuillez saisir votre second nombre entier : ');
+    $moyenne = ($nb1 + $nb2)/2;
+    echo ("La moyenne des nombres $nb1 et $nb2 est $moyenne.")
+?>
