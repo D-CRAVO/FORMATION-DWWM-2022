b13c0012030e89202689224f1c76ba80cefe66de
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-18 09:06:21 +0200

    Create ex_3-5_premier

---
+++ DOSSIER_05_PHP/ExercicesPHP/partie_3/ex_3-5_premier.php
@@ -0,0 +1,27 @@
+<!-- NOMBRE PREMIER -->
+
+<?php
+    $nb = (int)readline("Veuillez saisir un nombre entier : ");
+    if ($nb === 2)
+    {
+        $resultat = " 1 Le nombre est premier";
+    }
+    else
+    {
+        do
+        {
+            $i = 2;
+            $modulo = $nb % $i;
+            if ($modulo === 0)
+            {
+                $resultat = " 2 Le nombre n'est pas premier";
+            }
+            else
+            {
+                $resultat = " 3 Le nombre est premier";
+            }
+        }while ($modulo !== 0 && $i > $nb -1);
+
+    }
+    echo $resultat;
+?>
