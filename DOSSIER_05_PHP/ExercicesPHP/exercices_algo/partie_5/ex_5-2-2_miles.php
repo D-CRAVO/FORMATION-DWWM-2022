7bf3d36fa12db5b217ba722f71d8d5b77498bea4
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-06-20 21:47:48 +0200

    Update ex_5-2-2_miles.php

--- DOSSIER_05_PHP/ExercicesPHP/exercices_algo/partie_5/ex_5-2-2_miles.php
+++ DOSSIER_05_PHP/ExercicesPHP/exercices_algo/partie_5/ex_5-2-2_miles.php
@@ -4,13 +4,35 @@
     $valeur = readline("Veuillez entrer votre valeur suivi de son unité (mi ou km) : ");
     $parties = explode(' ',$valeur);// separe la chaine
 
-    $valeur = floatval($parties[0]); // pour faire la convertion
+    //$valeur = floatval($parties[0]); // pour faire la convertion
 
-    $unite = trim($parties[1]); // 
-    if ($unite === 'mi')
+    $distance = doubleval($parties[0]);
+    $unite = trim($parties[1]); // retire les espaces
+
+    if ($unite === 'mi') 
     {
-
+        $result = conversionMiKm($distance);
     }
-    
-    
-?>
+    else if ($unite === 'km') 
+    {
+        $result = conversionKmMi ($distance);
+    }
+    else 
+    {
+        $result = conversionKmMi ($distance);
+    }
+    echo $result;
+
+    function conversionMiKm ($distance)
+    {
+        $Km = doubleval($distance / 1.609);
+        return $distance . ' miles donne ' . $Km . ' kilomètres.';
+        //return "$distance miles donne $Km kilomètres.";
+    }
+
+    function conversionKmMi ($distance)
+    {
+        $Mi = doubleval($distance * 1.609);
+        return $distance . ' kilometres donne ' . $Mi . ' miles.';
+        //return "$distance kilometres donne $Mi miles.";
+    }
