a284e59c35c673e20653fd1b1f4c1b05f526ff87
David <toto@crm.fr>
2022-06-20 16:39:21 +0200

    Create ex_5-2-2_miles.php

---
+++ DOSSIER_05_PHP/ExercicesPHP/exercices_algo/partie_5/ex_5-2-2_miles.php
@@ -0,0 +1,16 @@
+<!-- CONVERSION KILOMETRES MILES -->
+
+<?php
+    $valeur = readline("Veuillez entrer votre valeur suivi de son unité (mi ou km) : ");
+    $parties = explode(' ',$valeur);// separe la chaine
+
+    $valeur = floatval($parties[0]); // pour faire la convertion
+
+    $unite = trim($parties[1]); // 
+    if ($unite === 'mi')
+    {
+
+    }
+    
+    
+?>
