e1ce0b00e8e7fcd8d72a1a1cf4e53d20c11579a1
David <toto@crm.fr>
2022-06-21 09:52:06 +0200

    Delete ex_2A.php

--- DOSSIER_05_PHP/ExercicesPHP/exercices_php_cours/serie_2/ex_2A.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getSum (int $nb1, int $nb2) : int
-{
-    return $nb1 + $nb2;
-}
-$nb1 = readline('Veuillez saisir le nombre 1 : ');
-$nb2 = readline('Veuillez saisir le nombre 2 : ');
-echo 'La somme de '. $nb1 . ' et de ' . $nb2 . ' est de ' . getSum($nb1, $nb2);
