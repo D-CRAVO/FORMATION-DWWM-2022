db689b4fa90f9b23cee030668ac0e14ffeed79ab
David <toto@crm.fr>
2022-06-29 16:56:50 +0200

    Create financier

---
+++ DOSSIER_04_OBJET/exercices_objets/financier/AppFinancier/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using financier;
+
+namespace AppFinancier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compte compteTintin = new Compte(1234, "Tintin", 400, -500);
+            Compte compteHaddock = new Compte(456, "Haddock", 0, -2000);
+            Console.WriteLine("-------------------avant virement ---------------");
+            Console.WriteLine(compteTintin.ToString());
+            Console.WriteLine(compteHaddock);
+            Console.WriteLine("-------------------après virement --------------");
+            if (compteTintin.Transferer(1200,compteHaddock))
+            {
+                Console.WriteLine(compteTintin.ToString());
+                Console.WriteLine(compteHaddock);
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible, dépassement découvert autorisé.");
+            }
+            Console.WriteLine("-------------------avant credit débit---------------");
+            Console.WriteLine(compteTintin.ToString());
+            Console.WriteLine(compteHaddock);
+            compteTintin.Crediter(2000);
+            compteHaddock.Debiter(500);
+            Console.WriteLine("-------------------après credit débit---------------");
+            Console.WriteLine(compteTintin.ToString());
+            Console.WriteLine(compteHaddock);
+            
+
+            if (compteTintin.Superieur(compteHaddock))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Le solde du compte de Tintin est supérieur au solde du compte de Haddock");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Le solde du compte de Tintin est inférieur au solde du compte de Haddock");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DOSSIER_04_OBJET/exercices_objets/financier/financier/Banque.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financier
+{
+   public class Banque
+    {
+        // Champs ou attributs
+        private Compte[] mesComptes;
+        private int nbComptes;
+        private string nom;
+        private string ville;
+
+        // Propriétés
+
+        public string Nom { get => nom; }
+        public string Ville { get => ville; }
+
+
+        // Constructeur
+        public Banque(Compte[] _mesComptes, int _nbComptes, string _nom, string _ville)
+        {
+            this.mesComptes = _mesComptes;
+            this.nbComptes = _nbComptes;
+            this.nom = _nom;  
+            this.ville = _ville;
+        }
+
+        public Banque()
+        {
+            this.mesComptes = null;
+            this.nbComptes = 0;
+            this.nom = "sans_nom";
+            this.ville = "sans_ville";
+        }
+
+        private void AjouteCompte(Compte _unCompte)
+        {
+            nbComptes++;
+        }
+
+        public void AjouteCompte(int _numero, string _nom, int _solde, int _decouvertAutorise)
+        {
+
+        }
+    }
+}
