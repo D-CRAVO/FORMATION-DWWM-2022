7772361a31f540942083fd956046305104f6e3df
David <toto@crm.fr>
2022-06-29 09:45:17 +0200

    Update index.php

--- DOSSIER_05_PHP/ExercicesPHP/exercices_grafikart/premiere_classe/index.php
+++ DOSSIER_05_PHP/ExercicesPHP/exercices_grafikart/premiere_classe/index.php
@@ -3,13 +3,17 @@
 require 'Personnage.php';
 
 $merlin = new Personnage("Merlin");
-//$merlin->nom = "Merlin";
 $merlin->regenerer();
-
 $harry = new Personnage("Harry");
-//$harry->nom = "Harry";
-$harry->mort();
+
+$merlin->attaque($harry);
 
 
+
+if($harry->mort()){
+    echo 'Harry est mort' . PHP_EOL;
+}else{
+    echo 'Harry a survécu' . PHP_EOL;
+}
 var_dump($merlin);
 var_dump($harry);
