b4f6d43684258adbd336274f1493a8013d7fd0da
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-03 20:25:19 +0200

    Update index.php

--- DOSSIER_04_OBJET/exercices_objets/compteBancaire/index.php
+++ DOSSIER_04_OBJET/exercices_objets/compteBancaire/index.php
@@ -1,6 +1,59 @@
 <?php
 
-require 'Compte.php';
-$compte01 = new Compte(1, 'David CRAVO', 10000, 500);
+//require 'Compte.php';
+require 'Banque.php';
 
+/* $compte01 = new Compte(1, 'David CRAVO', 10000, 500);
+$compte02 = new Compte(2, 'David CRAVO', 1000, 250);
+
+echo '*** avant credit ***' . PHP_EOL;
 echo $compte01 . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+$compte02->crediter(500);
+
+echo '*** après crédit ***' . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+$compte01->debiter(11000);
+
+echo '*** après débit ***' . PHP_EOL;
+echo $compte01 . PHP_EOL;
+
+echo '*** avant transfert 1 ***' . PHP_EOL;
+echo $compte01 . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+$compte02->transferer($compte01, 1750);
+
+echo '*** après transfert 1 ***' . PHP_EOL;
+echo $compte01 . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+echo '*** avant transfert 2 ***' . PHP_EOL;
+echo $compte01 . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+$compte02->transferer($compte01, 1050);
+
+echo '*** après transfert 2 ***' . PHP_EOL;
+echo $compte01 . PHP_EOL;
+echo $compte02 . PHP_EOL;
+
+$compte01->comparer($compte02); */
+
+$compte03 = new Compte(3, "Toto MACHIN", 2500, 350);
+$compte04 = new Compte(4, "Toto MACHIN", 11500, 500);
+$compte05 = new Compte(5, "Titi BIDULE", 500, 0);
+
+$cmBlois = new Banque('Crédit Mutuel', 'Blois');
+$cmBlois->premierCompte($compte03);
+$cmBlois->ajouterCompte($compte04);
+$cmBlois->ajouterCompte($compte05);
+
+//echo($cmBlois);
+//var_dump($cmBlois);
+$cmBlois->compteSup();
+//var_dump($cmBlois->mesComptes[1]);
+
+//echo($cmBlois->rendCompte());
