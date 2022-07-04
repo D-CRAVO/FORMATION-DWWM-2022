0a36c62bb2271cefc7bc60b1ed94568759f65fbf
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-04 22:43:11 +0200

    Update index.php

--- DOSSIER_04_OBJET/exercices_objets/point/index.php
+++ DOSSIER_04_OBJET/exercices_objets/point/index.php
@@ -6,11 +6,18 @@ $nom = 'A';
 $abscisseXA = 8;
 $ordonneeYA = 8;
 $pointA = new Point($nom, $abscisseXA, $ordonneeYA);
-$pointB = new Point('B', 4, 4);
-$pointC = new Point('C');
+$pointB = new Point('B', 4, 2);
+$pointC = new Point('C', 4, 1);
+$pointD = new Point('D');
 
 echo $pointA;
 echo $pointB;
 echo $pointC;
-echo ($pointA->deplacer(12, 12));
+echo $pointD;
+echo ('deplacer(12, 12)      : ' . $pointA->deplacer(12, 12));
 echo $pointA;
+
+echo ('symetrieAxeAbscisse() : ' . $pointA->symetrieAxeAbscisse());
+echo ('symetrieAxeOrdonnee() : ' . $pointA->symetrieAxeOrdonnee());
+echo ('symetrieOrigine()     : ' . $pointB->symetrieOrigine());
+echo ('symetrieBissectrice() : ' . $pointC->symetrieBissectrice());
