c683d3078ad876aee1340daa7e54a2a13f10ad87
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-04 22:43:07 +0200

    Create index.php

---
+++ DOSSIER_04_OBJET/exercices_objets/fraction/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'Fraction.php';
+
+echo ('Fraction(12,7) : ' . $fraction1 = new Fraction(12,7));
+echo ('Fraction(9,1)  : ' . $fraction2 = new Fraction(9,1));
+echo ('Fraction(0,1)  : ' . $fraction3 = new Fraction(0,1));
+echo ('Fraction(1,0)  : ' . $fraction4 = new Fraction(1,0));
+echo ('Fraction(0,0)  : ' . $fraction5 = new Fraction(0,0));
+echo ('Fraction(4,7)  : ' . $fraction6 = new Fraction(4,7));
+
+echo ('Opposé de Fraction(4,7)  : ' . $fraction6->oppose());
+echo ('Opposé de Fraction(4,7)  : ' . $fraction6->oppose());
+echo ('Inverse de Fraction(4,7) : ' . $fraction6->inverse());
