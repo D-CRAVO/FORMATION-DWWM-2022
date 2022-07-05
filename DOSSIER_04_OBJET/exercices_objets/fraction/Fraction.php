474993757bdb574f245a2f8f5703670ff044adca
David <toto@crm.fr>
2022-07-05 16:53:05 +0200

    Update Fraction.php

--- DOSSIER_04_OBJET/exercices_objets/fraction/Fraction.php
+++ DOSSIER_04_OBJET/exercices_objets/fraction/Fraction.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Classe Fraction
+ * @author David CRAVO <contact@davidcravo.fr>
+ */
 class Fraction
 {
     /**
@@ -98,11 +101,135 @@ class Fraction
     }
 
     /**
+     * Détermine si la fraction à comparer est supérieure à la fraction $fraction
+     *
+     * @param Fraction $fraction
+     * @return boolean
+     */
+    public function superieurA(Fraction $fraction) : string
+    {
+        if ($this->denominateur !== 0 && $fraction->denominateur !== 0)
+        {
+            $numeriqueA = $this->numerateur / $this->denominateur;
+            $numerique = $fraction->numerateur / $fraction->denominateur;
+            $result = $numeriqueA > $numerique ? 'true' : 'false';
+        }
+        else
+        {
+            $result = 'Le dénominateur ne peut être égal à 0';
+        }
+        return $result;
+    }
+
+    /**
+     * Vérifie si la fraction à comparer est égale à la fraction $fraction 
+     *
+     * @param Fraction $fraction
+     * @return void
+     */
+    public function egalA(Fraction $fraction)
+    {
+        if ($this->denominateur !== 0 && $fraction->denominateur !== 0)
+        {
+            $numeriqueA = $this->numerateur / $this->denominateur;
+            $numerique = $fraction->numerateur / $fraction->denominateur;
+            $result = $numeriqueA === $numerique ? 'true' : 'false';
+        }
+        else
+        {
+            $result = 'Le dénominateur ne peut être égal à 0';
+        }
+        return $result;
+    }
+
+    /**
+     * Calcule le plus grand denomitateur commun
+     * des numérateur et dénominateur
+     *
+     * @return integer
+     */
+    public function getPgcd() : int
+    {
+        $a = $this->numerateur;
+        $b = $this->denominateur;
+        $pgcd = 1;
+        if ($a !== 0 && $b !== 0)
+        {
+            $a = $a < 0 ? -$a : $a;
+            $a = $a < 0 ? -$a : $a;
+            while ($a != $b)
+            if ($a < $b)
+            {
+                $b = $b - $a;
+            }
+            else
+            {
+                $a = $a - $b;
+            }
+            $pgcd = $a;
+        }
+        return $pgcd;
+    }
+
+    /**
+     * Réduit la fraction à réduire en fonction 
+     * à l'aide de la fonction getPgcd
+     *
+     * @return Fraction
+     */
+    public function reduire() : Fraction
+    {
+        $pgcd = $this->getPgcd();
+        $this->numerateur /= $pgcd;
+        $this->denominateur /= $pgcd;
+        return $this; 
+    }
+
+    /**
+     * Additionne 2 fractions
+     * et réduit la fraction obtenue
+     *
+     * @param Fraction $fraction
+     * @return Fraction
+     */
+    public function plus(Fraction $fraction) : Fraction
+    {
+        $this->numerateur *= $fraction->denominateur;
+        $fraction->numerateur *= $this->denominateur;
+        $numerateur = $this->numerateur + $fraction->numerateur;
+        $denominateur = $this->denominateur * $fraction->denominateur;
+        $fractionPlus = new Fraction($numerateur, $denominateur);
+        $fractionReduite = $fractionPlus->reduire();
+        return $fractionReduite;
+    }
+
+    /**
+     * Soustrait la fraction $fraction à la fraction sélectionnée
+     * et réduit la fraction obtenue
+     *
+     * @param Fraction $fraction
+     * @return Fraction
+     */
+    public function moins(Fraction $fraction) : Fraction
+    {
+        $this->numerateur *= $fraction->denominateur;
+        $fraction->numerateur *= $this->denominateur;
+        $numerateur = $this->numerateur - $fraction->numerateur;
+        $denominateur = $this->denominateur * $fraction->denominateur;
+        $fractionMoins = new Fraction($numerateur, $denominateur);
+        $fractionReduite = $fractionMoins->reduire();
+        return $fractionReduite;
+    }
+
+    //public function multiplie()
+
+
+    /**
      * Affichage
      *
      * @return string
      */
-    public function __toString() : string
+    public function __toString()
     {
         if ($this->denominateur === 0)
         {
