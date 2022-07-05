c07da31f858969b327fc9ff0de0c36922261943d
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-05 23:52:36 +0200

    Update index.php

--- DOSSIER_04_OBJET/exercices_objets/fraction/index.php
+++ DOSSIER_04_OBJET/exercices_objets/fraction/index.php
@@ -7,14 +7,14 @@ require 'Fraction.php';
 // echo ('Fraction(0,1)     : ' . $fraction3 = new Fraction(0,1));
 // echo ('Fraction(1,0)     : ' . $fraction4 = new Fraction(1,0));
 // echo ('Fraction(0,0)     : ' . $fraction5 = new Fraction(0,0));
-// echo ('Fraction(4,7)     : ' . $fraction6 = new Fraction(4,7));
-// echo ('Fraction(120,40)  : ' . $fraction7 = new Fraction(120,40));
-// echo ('Fraction(135,40)  : ' . $fraction8 = new Fraction(135,40));
+echo ("Fraction(4,7)     : " . $fraction6 = new Fraction(4,-7));
+echo ('Fraction(120,40)  : ' . $fraction7 = new Fraction(-120,40));
+echo ('Fraction(135,40)  : ' . $fraction8 = new Fraction(-135,-40));
 echo ('Fraction(135,40)  : ' . $fraction9 = new Fraction(135,40));
 echo ('Fraction(4,7)     : ' . $fraction10 = new Fraction(4,7));
 echo ('Fraction(1105,280): ' . $fraction11 = new Fraction(1105,280));
-echo ('Fraction (20,3)   : ' . $fraction12 = new Fraction(20,3));
-echo ('Fraction (12,3)   : ' . $fraction13 = new Fraction(12,3));
+echo ('Fraction (20,3)   : ' . $fraction12 = new Fraction(20,-3));
+echo ('Fraction (12,3)   : ' . $fraction13 = new Fraction(12,-3));
 
 echo PHP_EOL;
 
@@ -36,10 +36,20 @@ echo PHP_EOL;
 // echo PHP_EOL;
 
 // echo ('Pgcd 120 et 40 : ' . $fraction7->getPgcd(). PHP_EOL);
-// echo ('Réduction de Fraction(120,40) : ') . $fraction7->reduire() . PHP_EOL;
-// echo ('Réduction de Fraction(135,40) : ') . $fraction8->reduire() . PHP_EOL;
-// echo ('Réduction de Fraction(1105,280) : ') . $fraction11->reduire() . PHP_EOL;
-// echo PHP_EOL;
+echo ('Réduction de Fraction(120,40) : ') . $fraction7->reduire() . PHP_EOL;
+echo ('Réduction de Fraction(135,40) : ') . $fraction8->reduire() . PHP_EOL;
+echo ('Réduction de Fraction(1105,280) : ') . $fraction11->reduire() . PHP_EOL;
+echo PHP_EOL;
 
-echo ('Fraction(135,40) plus Fraction(4, 7) : ' . $fraction9->plus($fraction10));
-echo ('Fraction(20,3) moins Fraction(12,3) : ' . $fraction12->moins($fraction13));
+//echo ('Fraction(135,40) plus Fraction(4, 7) : ' . $fraction9->plus($fraction10));
+//echo ('Fraction(20,3) moins Fraction(12,3) : ' . $fraction12->moins($fraction13));
+
+echo PHP_EOL;
+echo $fraction12;
+echo $fraction13;
+echo ('Fraction(-20,3) multiplé par Fraction(-12,3) : ' . $fraction12->multiplier($fraction13));
+
+echo PHP_EOL;
+echo $fraction12;
+echo $fraction13;
+echo ('Fraction(-20,3) divisé par Fraction(-12,3) : ' . $fraction12->diviser($fraction13));
