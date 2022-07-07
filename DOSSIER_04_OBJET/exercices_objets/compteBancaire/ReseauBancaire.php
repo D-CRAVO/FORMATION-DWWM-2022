5ba9b94f751c51047e9f043671b2f8b4990179b8
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-07 21:04:12 +0200

    Create ReseauBancaire.php

---
+++ DOSSIER_04_OBJET/exercices_objets/compteBancaire/ReseauBancaire.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'Banque.php';
+
+class ReseauBancaire
+{
+
+    private string $nom;
+
+    private array $nbBanques;
+
+    private array $mesBanques; 
+
+    public function __construct()
+    {
+        $this->nom = $nom;
+        $this->nbBanques = $nbBanques;
+        $this->mesBanques = [];
+    }
+
+
+
+
+}
+
