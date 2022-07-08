b78f54072e31da4a9c2d685bf508cb46ded8dca0
David <toto@crm.fr>
2022-07-08 11:49:22 +0200

    Update ReseauBancaire.php

--- DOSSIER_04_OBJET/exercices_objets/compteBancaire/ReseauBancaire.php
+++ DOSSIER_04_OBJET/exercices_objets/compteBancaire/ReseauBancaire.php
@@ -11,15 +11,49 @@ class ReseauBancaire
 
     private array $mesBanques; 
 
-    public function __construct()
+    public function setNomReseauBancaire(string $nom) : void
     {
         $this->nom = $nom;
-        $this->nbBanques = $nbBanques;
-        $this->mesBanques = [];
     }
 
+    public function getNomReseauBancaire() : string
+    {
+        return $this->nom;
+    }
 
+    public function __construct(string $nom)
+    {
+        $this->setNomReseauBancaire($nom);
+        $this->mesReseauxBancaires = [];
+    }
 
+    public function __toString() : string
+    {
+        //$nbBanques = count($this->mesBanques);
+        $result = PHP_EOL . 'Le Réseau bancaire s\'appelle : ' . $this->nom. PHP_EOL;
 
+        // for ($i=0; $i < $nbBanques; $i++) 
+        // { 
+        //     $result .= PHP_EOL . $this->mesBanques[$i]->__toString() .PHP_EOL;
+        // }
+        
+        return $result;
+    }
+
+    public function AjouterBanque(Banque $banque) : void
+    {
+        array_push($this->mesReseauxBancaires, $banque);
+    }
+
+    // public function AffichageMesComptes() : string
+    // {
+    //     $result = 'Le compte bancaire ' . $this->nom . 'possède les comptes suivant : ';
+    //     for ($i=0; $i < count($this->mesReseauxBancaires); $i++) 
+    //     { 
+    //         $result .= PHP_EOL . $this->mesReseauxBancaires[$i]->__toString() .PHP_EOL;
+    //     }
+    //     return $result;
+    // }
 }
+
 
