434a82d20b608125b55213d9df704d99e70b2b8e
David <toto@crm.fr>
2022-07-18 16:58:33 +0200

    Create Cheval.php

---
+++ DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/Cheval.php
@@ -0,0 +1,12 @@
+<?php
+
+Class Cheval extends Herbivore
+{
+    public function manger() : string
+    {
+        return parent::manger() . ' de l\'herbe et du foin.';
+    }
+
+    public function 
+
+}
