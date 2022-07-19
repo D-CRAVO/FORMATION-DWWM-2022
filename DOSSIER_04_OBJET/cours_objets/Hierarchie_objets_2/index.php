84f569ce6d93f18ac118938987773a64a2dca05c
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-19 07:09:43 +0200

    Update index.php

--- DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/index.php
+++ DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/index.php
@@ -2,3 +2,12 @@
 
 require '.\Animal.php';
 require '.\Herbivore.php';
+require '.\Cheval.php';
+require '.\Girafe.php';
+
+/** Création de nouveaux animaux */
+
+$carnivore = new Animal;
+$omnivore = new Animal;
+$frugivore = new Animal;
+$insectivore = new Animal;
