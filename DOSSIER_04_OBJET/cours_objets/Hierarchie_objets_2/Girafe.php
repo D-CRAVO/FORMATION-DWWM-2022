ccf7d5eb6f33101614a72fefcaafe9e38d5a4d44
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-24 20:51:28 +0200

    Update Girafe.php

--- DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/Girafe.php
+++ DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/Girafe.php
@@ -2,5 +2,13 @@
 
 class Girafe extends Herbivore
 {
-    
+   public function seDeplacer() : string
+   {
+        return parent::seDeplacer() . 'doucement.';
+   } 
+
+   public function crier() : string
+   {
+        return parent::crier() . 'en meuglant.';
+   }
 }
