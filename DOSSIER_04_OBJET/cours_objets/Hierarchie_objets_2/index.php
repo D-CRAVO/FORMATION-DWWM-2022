156bff225e8317a36c5c4e7468b6b8c2f1e04936
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-24 20:51:39 +0200

    Update index.php

--- DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/index.php
+++ DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/index.php
@@ -1,13 +1,41 @@
 <?php
 
-require '.\Animal.php';
-require '.\Herbivore.php';
-require '.\Cheval.php';
-require '.\Girafe.php';
+require './Animal.php';
+require './Herbivore.php';
+require './Cheval.php';
+require './Girafe.php';
+require './Chien.php';
 
 /** Création de nouveaux animaux */
 
-$carnivore = new Animal;
-$omnivore = new Animal;
-$frugivore = new Animal;
-$insectivore = new Animal;
+$carnivore = new Animal('Carnivore');
+
+/** Création de nouveaux chevaux */
+
+$cheval = new Cheval('Cheval');
+
+/** Création de nouvelles girafes */
+
+$girafe = new Girafe('Girafe');
+
+/** Création de nouveaux chiens */
+
+$chien = new Chien('Carnivore', 'Chien');
+
+/** Afichage */
+
+echo $carnivore->seDeplacer() . PHP_EOL; 
+echo $carnivore->manger() . PHP_EOL; 
+echo $carnivore->crier() . PHP_EOL; 
+
+echo $cheval->seDeplacer() . PHP_EOL;
+echo $cheval->manger() . PHP_EOL;
+echo $cheval->crier() . PHP_EOL;
+
+echo $girafe->seDeplacer() . PHP_EOL;
+echo $girafe->manger() . PHP_EOL;
+echo $girafe->crier() . PHP_EOL;
+
+echo $chien->seDeplacer() . PHP_EOL;
+echo $chien->manger() . PHP_EOL;
+echo $chien->crier() . PHP_EOL;
