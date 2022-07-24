e28835722fd78b996ae842dc249404e8617f1cb7
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-07-24 20:51:21 +0200

    Update Cheval.php

--- DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/Cheval.php
+++ DOSSIER_04_OBJET/cours_objets/Hierarchie_objets_2/Cheval.php
@@ -4,11 +4,11 @@ Class Cheval extends Herbivore
 {
     public function manger() : string
     {
-        return parent::manger() . ' de l\'herbe et du foin.';
+        return parent::manger() . 'de l\'herbe et du foin.';
     }
 
     public function crier() : string
     {
-        return parent::crier() . ' en hennissant.';
+        return parent::crier() . 'en hennissant.';
     }
 }
