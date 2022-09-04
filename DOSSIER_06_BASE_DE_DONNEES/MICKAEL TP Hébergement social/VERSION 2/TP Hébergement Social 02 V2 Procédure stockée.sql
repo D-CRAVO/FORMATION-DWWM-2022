4efc836f4e4e60db7244db71134c1334b352012b
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2022-09-04 21:28:18 +0200

    Create TP Hébergement Social 02 V2 Procédure stockée.sql

---
+++ DOSSIER_06_BASE_DE_DONNEES/MICKAEL TP Hébergement social/VERSION 2/TP Hébergement Social 02 V2 Procédure stockée.sql
@@ -0,0 +1,38 @@
+-- procédure stockée
+CREATE OR REPLACE PROCEDURE create_resident 
+(
+	person_id INT
+	,person_firstname VARCHAR(50)
+	,person_lastname VARCHAR(100)
+	,person_birthdate DATE
+	,date_arrival TIMESTAMP
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN 
+
+INSERT INTO people
+(
+	person_id
+ 	,person_lastname
+	,person_firstname
+	,person_birthdate
+	,person_role_id
+)
+VALUES
+(person_id, person_lastname, person_firstname, person_birthdate, 1)
+;
+
+INSERT INTO residents
+(
+	resident_id
+	,date_arrival
+)
+VALUES
+(person_id, date_arrival)
+;
+
+END
+;
+$$
+
