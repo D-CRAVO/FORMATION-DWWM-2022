ecaf8941b473b1522b77af0451a40109ed2d34e5
David <toto@crm.fr>
2022-09-12 16:55:43 +0200

    Update employees_select.sql

--- DOSSIER_06_BASE_DE_DONNEES/MICKAEL Exercices SQL/EXERCICE 2 Employees/employees_select.sql
+++ DOSSIER_06_BASE_DE_DONNEES/MICKAEL Exercices SQL/EXERCICE 2 Employees/employees_select.sql
@@ -22,8 +22,8 @@ SELECT
 	,emp_hiredate
 	,emp_manager_id
 FROM employees
-WHERE emp_manager_id IS NOT null -- J'estime que le 'directeur' ne doit pas faire parti de la recherche
-ORDER BY emp_manager_id ASC, emp_lastname
+-- WHERE emp_manager_id IS NOT null -- Si j'estime que le 'directeur' ne doit pas faire parti de la recherche
+ORDER BY emp_manager_id ASC, emp_lastname ASC
 ;
 
 
@@ -32,17 +32,29 @@ ORDER BY emp_manager_id ASC, emp_lastname
 -- Trier la liste par nom de manager (ordre croissant) puis par date d’embauche de la plus récente à la plus ancienne.
 
 SELECT 
-	p1.emp_lastname
-	,p1.emp_salary
-	,p1.emp_hiredate
-	,p2.emp_lastname AS manager_lastname
-	,p2.emp_firstname AS manager_firstname
+	EM.emp_lastname
+	,EM.emp_salary
+	,EM.emp_hiredate
+	,MA.emp_lastname AS manager_lastname
+	,MA.emp_firstname AS manager_firstname
 FROM employees
-	INNER JOIN employees AS p1 ON employees.emp_id = p1.emp_id
-	INNER JOIN employees AS p2 ON employees.emp_manager_id = p2.emp_id
-ORDER BY manager_lastname ASC, p1.emp_hiredate DESC
+	INNER JOIN employees AS EM ON employees.emp_id = EM.emp_id
+	INNER JOIN employees AS MA ON employees.emp_manager_id = MA.emp_id
+ORDER BY manager_lastname ASC, EM.emp_hiredate DESC
 ;
 
+-- Autre syntaxe
+
+SELECT 
+	employee.emp_lastname
+	,employee.emp_salary
+	,employee.emp_hiredate
+	,manager.emp_lastname AS manager_lastname
+	,manager.emp_firstname AS manager_firstname
+FROM employees AS employee
+	INNER JOIN employees AS manager ON employee.emp_manager_id = manager.emp_id
+ORDER BY manager_lastname ASC, emp_hiredate DESC
+;
 
 -- 4
 -- Sélectionner les employés sans manager.
@@ -61,16 +73,14 @@ WHERE emp_manager_id IS null
 
 
 SELECT 
-	e1.emp_firstname
-	,e1.emp_lastname
-	,COUNT(e2.emp_id) AS resultat
-FROM employees
-	INNER JOIN employees AS e1 ON employees.emp_manager_id = e1.emp_id 
-	INNER JOIN employees AS e2 ON e2.emp_id = employees.emp_id
-GROUP BY e1.emp_firstname, e1.emp_lastname
+	MA.emp_firstname
+	,MA.emp_lastname
+	,COUNT(EM.emp_id) AS resultat
+FROM employees AS EM
+	INNER JOIN employees AS MA ON EM.emp_manager_id = MA.emp_id 
+GROUP BY MA.emp_firstname, MA.emp_lastname
 ORDER BY resultat ASC
 ;
-
 
 
 -- 6
@@ -78,13 +88,12 @@ ORDER BY resultat ASC
 -- Trier le résultat selon la valeur de la moyenne par ordre décroissant
 
 SELECT
-	e1.emp_firstname AS manager_firstname
-	,e1.emp_lastname AS manager_lastname
-	,e1.emp_id AS manager_id
-	,AVG(e2.emp_salary) AS moyenne
-FROM employees
-	INNER JOIN employees AS e1 ON employees.emp_manager_id = e1.emp_id
-	INNER JOIN employees AS e2 ON employees.emp_id = e2.emp_id
+	MA.emp_firstname AS manager_firstname
+	,MA.emp_lastname AS manager_lastname
+	,MA.emp_id AS manager_id
+	,AVG(EM.emp_salary) AS moyenne
+FROM employees AS MA
+	INNER JOIN employees AS EM ON EM.emp_manager_id = MA.emp_id
 GROUP BY manager_firstname, manager_lastname, manager_id
 ORDER BY moyenne DESC
 ;
@@ -140,5 +149,22 @@ FROM employees
 WHERE emp_manager_id IS NOT null
 ;
 
+--
+
+SELECT
+	MA.emp_id
+	,MA.emp_lastname
+	,MA.emp_firstname
+	,MA.emp_salary
+	,COUNT (EM.emp_id) AS nb_sub
+	,SUM(EM.emp_salary) AS total_salary
+	,AVG(EM.emp_salary) AS average_salary
+	,MIN(EM.emp_salary) AS poor_sub
+	,MAX(EM.emp_salary) AS rich_sub
+FROM employees AS MA
+	INNER JOIN employees AS EM ON EM.emp_manager_id IS NOT null
+WHERE MA.emp_manager_id IS null
+GROUP BY MA.emp_id
+;
 
 
