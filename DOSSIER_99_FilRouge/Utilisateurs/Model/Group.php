ee6546d070e4eef086edd2736b758ae051ffab28
CRAVO David Logement <contact@davidcravo.fr>
2022-10-05 07:12:19 +0200

    Update Group.php

--- DOSSIER_99_FilRouge/Utilisateurs/Model/Group.php
+++ DOSSIER_99_FilRouge/Utilisateurs/Model/Group.php
@@ -19,7 +19,23 @@ class Group
 
     public function getGroupName() : string
     {
-        
+        return $this->group_name;
+    }
+
+    private function setGroupName($_group_name) : void
+    {
+        $this->group_name = $_group_name;
+    }
+
+    public function __construct(int $_group_id, string $_group_name)
+    {
+        $this->setGroupId($_group_id);
+        $this->setGroupName($_group_name);
+    }
+
+    public function __toString() : string
+    {
+        $result = 'Le groupe ' . $this->group_id . ' a pour nom ' . $this->group_name . PHP_EOL;
     }
 }
 
