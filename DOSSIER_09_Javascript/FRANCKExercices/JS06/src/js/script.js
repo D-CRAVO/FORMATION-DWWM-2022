1931d8e05f984d1f9fb79ee4ac267b4adbf4aefa
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-11-08 16:57:49 +0100

    Création de l'exercice Javascript JS

---
+++ DOSSIER_09_Javascript/FRANCKExercices/JS06/src/js/script.js
@@ -0,0 +1,10 @@
+let qte01 = parseInt(document.getElementById('#qte01'));
+//qte01.addEventListener("keypress", prix())
+console.log(qte01);
+let unitaire01 = document.querySelector('#unitaire01');
+console.log(typeof(unitaire01));
+
+
+function prix(){
+    
+}
