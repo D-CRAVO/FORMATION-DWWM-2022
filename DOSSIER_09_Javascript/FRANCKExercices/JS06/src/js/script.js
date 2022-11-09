71b50d594ddc34dd91a42df8be05d25ba2726823
CRAVO David Logement <contact@davidcravo.fr>
2022-11-09 07:45:49 +0100

    Update script.js

--- DOSSIER_09_Javascript/FRANCKExercices/JS06/src/js/script.js
+++ DOSSIER_09_Javascript/FRANCKExercices/JS06/src/js/script.js
@@ -1,6 +1,9 @@
-let qte01 = parseInt(document.getElementById('#qte01'));
+let qte01 = parseInt(document.getElementById('qte01'));
 //qte01.addEventListener("keypress", prix())
 console.log(qte01);
+/* let unitaire01 = document.querySelector('#unitaire01').addEventListener("keypress", function() {
+    console.log('essai')
+}); */
 let unitaire01 = document.querySelector('#unitaire01');
 console.log(typeof(unitaire01));
 
