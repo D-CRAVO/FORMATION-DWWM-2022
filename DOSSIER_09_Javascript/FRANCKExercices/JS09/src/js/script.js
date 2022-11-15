0a591ce880296c78606a0f6377649f3064c1d636
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-11-15 16:56:04 +0100

    Exercice complété

--- DOSSIER_09_Javascript/FRANCKExercices/JS09/src/js/script.js
+++ DOSSIER_09_Javascript/FRANCKExercices/JS09/src/js/script.js
@@ -18,8 +18,55 @@ document.querySelector("#btnRouge").addEventListener("click", function(){
     colorierFond();
 });
 
+document.querySelector("#btnVert").addEventListener("click", function(){
+    document.querySelector("#inRouge").value = "00";
+    document.querySelector("#inVert").value = "ff";
+    document.querySelector("#inBleu").value = "00";
+    colorierFond();
+});
+
+document.querySelector("#btnBleu").addEventListener("click", function(){
+    document.querySelector("#inRouge").value = "00";
+    document.querySelector("#inVert").value = "00";
+    document.querySelector("#inBleu").value = "ff";
+    colorierFond();
+});
+
+document.querySelector("#btnHex").addEventListener("click", function(){
+    rouge = document.querySelector("#inRouge").value;
+    vert = document.querySelector("#inVert").value ;
+    bleu = document.querySelector("#inBleu").value;
+    colorierFondHex(rouge, vert, bleu);
+} );
+
+document.querySelector("#btnRgb").addEventListener("click", function(){
+    rouge = document.querySelector("#inRouge").value;
+    vert = document.querySelector("#inVert").value ;
+    bleu = document.querySelector("#inBleu").value;
+    colorierFondRgb(rouge, vert, bleu);
+});
+
+console.log(document.querySelectorAll("input[type=text]"))
+
+document.querySelectorAll("input[type=text]").addEventListener("blur", function(){
+    rouge = document.querySelector("#inRouge").value;
+    vert = document.querySelector("#inVert").value;
+    bleu = document.querySelector("#inBleu").value;
+    colorierFondRgb(rouge, vert, bleu);
+});
+
 function colorierFond(){
     let chaine = '#'+ document.getElementById("inRouge").value + document.getElementById("inVert").value + document.getElementById("inBleu").value;
     console.log(chaine);
-    document.getElementById("test").setAttribute("style", "background-color:" + chaine + ";");
+    document.getElementById("body").setAttribute("style", "background-color:" + chaine + ";");
+}
+
+function colorierFondHex(rouge, vert, bleu){
+    let chaine = '#'+ rouge + vert + bleu;
+    console.log(chaine);
+    document.getElementById("body").setAttribute("style", "background-color:" + chaine + ";");
+}
+
+function colorierFondRgb(rouge, vert, bleu){
+    document.querySelector("body").setAttribute("style", `background-color: rgb(${rouge}, ${vert}, ${bleu})`);
 }
