746467f9057f6b7a1d8123d43ccd6090e3b43aac
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-11-23 09:59:59 +0100

    Ajout de la Card affichant les attributs du Max Attack

--- DOSSIER_09_Javascript/FRANCKExercices/AJAX01/js/script.js
+++ DOSSIER_09_Javascript/FRANCKExercices/AJAX01/js/script.js
@@ -39,15 +39,17 @@ function creerTHead(data){
     }
 }
 
+
 /**
+ * Calcul permettant de déterminer
+ * quelle est la carte avec le plus de points d'attaque
  * 
  * @param {array} data 
- * @returns 
  */
 function maxAttack(data){
-    let valueMax = 0;
+    let valueMax = data[0].attack;
     let keyMax = 0;
-    for(let i=0; i<data.length; i++){
+    for(let i=1; i<data.length; i++){
         if(data[i].attack > valueMax){
             valueMax = data[i].attack;
             keyMax = i;
@@ -59,16 +61,19 @@ function maxAttack(data){
 
 
 /**
+ * Calcul permettant de déterminer
+ * quelle est la carte avec le plus de points d'armure
  * 
  * @param {array} data 
+ * @param {string} attribute 
  * @returns 
  */
-function maxArmor(data){
-    let valueMax = 0;
+function maxAttribute(data, attribute){
+    let valueMax = data[0].attribute;
     let keyMax = 0;
-    for(let i=0; i<data.length; i++){
-        if(data[i].armor > valueMax){
-            valueMax = data[i].armor;
+    for(let i=1; i<data.length; i++){
+        if(data[i].attribute > valueMax){
+            valueMax = data[i].attribute;
             keyMax = i;
         }
     }
@@ -76,22 +81,36 @@ function maxArmor(data){
     return keyMax;
 }
 
-
+/**
+ * Intègre les informations à la carte d'affichage.
+ * 
+ * @param {Object} data 
+ * @param {integer} maxAttackKey 
+ */
+function showMaxAttack(data, maxAttackKey){
+    document.querySelector("#attackId").innerText = data[maxAttackKey].id;;
+    document.querySelector("#attackName").innerText = data[maxAttackKey].name;
+    document.querySelector("#attackPlayedValue").innerText = data[maxAttackKey].played;
+    document.querySelector("#attackVictoriesValue").innerText = data[maxAttackKey].victory;
+    document.querySelector("#attackPowerValue").innerText = data[maxAttackKey].power;
+    document.querySelector("#attackAttackValue").innerText = data[maxAttackKey].attack;
+    document.querySelector("#attackArmorValue").innerText = data[maxAttackKey].armor;
+}
 
 /**
  * Récupération du fichier json
  */
- let maxAttackData
+let maxAttackKey;
 fetch("https://arfp.github.io/tp/web/frontend/cardgame/cardgame.json")
         .then(response => { 
             return response.json();
         })
         .then(jsondata => {
-            jasonData = jsondata;
             creerBody(jsondata);
             creerTHead(jsondata);
-            maxAttackData = maxAttack(jsondata);
-            maxArmor(jsondata);
+            maxAttackKey = maxAttack(jsondata);
+            showMaxAttack(jsondata, maxAttackKey)
+            maxAttribute(jsondata, armor);
             // console.log(jsondata)
         });
         
