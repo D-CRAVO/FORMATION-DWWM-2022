3d925a5028d7704e0f730316949a75e448ee7963
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-19 17:52:01 +0100

    Réorganisation

---
+++ DOSSIER_03_Algorithmique/Exercices/ex13_moyenne/exercice_1-3_calcul-de-la-moyenne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex13_moyenne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Veuillez saisir un premier nombre : ");
+            float nombre1 = float.Parse(Console.ReadLine());
+            Console.WriteLine(nombre1);
+            Console.Write("Veuillez saisir un second nombre : ");
+            float nombre2 = float.Parse(Console.ReadLine());
+            Console.WriteLine(nombre2);
+            float moyenne = (nombre1 + nombre2) / 2;
+            Console.WriteLine("La moyenne de " + nombre1 + " et " + nombre2 + " est : " + moyenne);
+        }
+    }
+}
---
+++ DOSSIER_03_Algorithmique/Exercices/ex15_secteurCirculaire/exercice_1-5_surface-secteur-circulaire/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex15_secteurCirculaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
